f7e2e538e952b724249493177721e6c0bd3f38e3
Tyler Long <tyler4long@gmail.com>
2020-08-10 08:45:14 -0700

    First passing test case

--- src/test/java/com/ringcentral/engagevoice/EngageVoiceTest.java
+++ src/test/java/com/ringcentral/engagevoice/EngageVoiceTest.java
@@ -3,6 +3,7 @@ package com.ringcentral.engagevoice;
 import com.ringcentral.RestClient;
 import com.ringcentral.RestException;
 import com.ringcentral.engagevoice.definitions.EngageVoiceToken;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,18 +14,21 @@ import static org.junit.Assert.assertTrue;
 public class EngageVoiceTest {
     @Test
     public void authorize() throws IOException, RestException, com.ringcentral.engagevoice.RestException {
+        Dotenv dotenv = Dotenv.load();
+        
         RestClient rc = new RestClient(
-                System.getenv("RINGCENTRAL_CLIENT_ID"),
-                System.getenv("RINGCENTRAL_CLIENT_SECRET"),
-                System.getenv("RINGCENTRAL_SERVER_URL")
+                dotenv.get("ENGAGE_VOICE_CLIENT_ID"),
+                dotenv.get("ENGAGE_VOICE_CLIENT_SECRET"),
+                dotenv.get("ENGAGE_VOICE_RC_SERVER_URL")
         );
 
         rc.authorize(
-                System.getenv("RINGCENTRAL_USERNAME"),
-                System.getenv("RINGCENTRAL_EXTENSION"),
-                System.getenv("RINGCENTRAL_PASSWORD")
+                dotenv.get("ENGAGE_VOICE_USERNAME"),
+                dotenv.get("ENGAGE_VOICE_EXTENSION"),
+                dotenv.get("ENGAGE_VOICE_PASSWORD")
         );
-        EngageVoice engageVoice = new EngageVoice();
+
+        EngageVoice engageVoice = new EngageVoice(dotenv.get("ENGAGE_VOICE_SERVER_URL"));
         EngageVoiceToken evt = engageVoice.authorize(rc.token.access_token);
         assertNotNull(evt);
         assertNotNull(evt.accessToken);
